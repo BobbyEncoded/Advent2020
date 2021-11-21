73e0641fd0e99f445cfb4bb7302cf7d015b84968
Robert Zanone <rjzanone@gmail.com>
2021-11-21 13:08:51 -0500

    Day 16 Part 2 complete. General functions also added. File read changed to a general function in the namespace.

---
+++ Advent2020D7/Day16.fs
@@ -0,0 +1,176 @@
+﻿namespace Advent2020.Day16
+
+open System
+open Advent2020 //Contains Range type and accessors
+open System.Text.RegularExpressions //Allows usage of Regular Expressions
+
+module Main =
+
+    let findTicketScanningErrorRate (rules : Map<string,(Range<int> * Range<int>)>) (nearbyTickets : int list list) : int =
+        let allValidRanges : Range<int> list =
+            let (range1, range2) = 
+                rules
+                |> Map.toList
+                |> List.map snd
+                |> List.unzip
+            range2
+            |> List.append range1
+
+        let allNearbyTicketFields = 
+            nearbyTickets
+            |> List.concat
+
+        let checkIfFieldIsValid (rangesList : Range<int> list) (field : int) : bool = 
+            rangesList
+            |> List.map (fun x -> x.InRangeInclusive field)
+            |> List.contains true
+            
+        let valueForValidFields (field : int) = 
+            let validity = checkIfFieldIsValid allValidRanges field
+            if validity then 0 else field
+
+        allNearbyTicketFields
+        |> List.map valueForValidFields
+        |> List.sum
+
+    let findValidTickets (rules : Map<string,(Range<int> * Range<int>)>) (nearbyTickets : int list list) =
+        let allValidRanges : Range<int> list =
+            let (range1, range2) = 
+                rules
+                |> Map.toList
+                |> List.map snd
+                |> List.unzip
+            range2
+            |> List.append range1
+
+        let checkIfFieldIsValid (rangesList : Range<int> list) (field : int) : bool = 
+            rangesList
+            |> List.map (fun x -> x.InRangeInclusive field)
+            |> List.contains true
+
+        nearbyTickets
+        |> List.filter (fun x ->
+            x
+            |> List.map (checkIfFieldIsValid allValidRanges)
+            |> List.forall id
+            )
+
+    let matchFieldsToRules (rules : Map<string,(Range<int> * Range<int>)>) (validTickets : int list list) = // : string Set list = 
+        let ruleEntries = validTickets |> List.transpose //This is a grouping of all entries in an individual rule
+        //We will need to find which entry correlates to each grouping of rules
+        let findRulesTheEntriesMatch (entries : int list) : string Set =
+            let entryValidRules (entry : int) = 
+                rules
+                |> Map.filter(fun _ ranges ->
+                    let range1, range2 = ranges
+                    range1.InRangeInclusive(entry) || range2.InRangeInclusive(entry)
+                    )
+                |> Map.keys
+                |> Set.ofSeq
+            entries
+            |> List.map entryValidRules
+            |> Set.intersectMany
+
+        let validRuleConfigs = 
+            ruleEntries
+            |> List.map findRulesTheEntriesMatch
+
+        let getAllSingletonEntries (validRuleConfigs : string Set list) : string list =
+            let ruleMatchInit = validRuleConfigs |> List.map (fun _ -> "") //Empty list of strings to hold the field name
+            let rec removeSingletonElements (currentValidRules : string list) (currentPossibleRules : string Set list) = 
+                let filledList = currentValidRules |> List.contains ""
+                match filledList with 
+                | false -> (currentValidRules, currentPossibleRules)
+                | true ->
+                    let singletonRuleIndex = 
+                        currentPossibleRules
+                        |> List.findIndex (fun x -> x.Count = 1)
+                    let element =
+                        currentPossibleRules
+                        |> List.item singletonRuleIndex
+                        |> Set.toList
+                        |> List.exactlyOne
+                    let newValidRules =
+                        currentValidRules
+                        |> List.updateAt singletonRuleIndex element
+                    let newCurrentPossibleRules = 
+                        currentPossibleRules
+                        |> List.map (Set.remove element)
+                    removeSingletonElements newValidRules newCurrentPossibleRules
+            let (rulesList, _) = removeSingletonElements ruleMatchInit validRuleConfigs
+            rulesList
+
+        //printfn "%A" (getAllSingletonEntries validRuleConfigs)
+
+        getAllSingletonEntries validRuleConfigs |> List.indexed
+
+    let departureProduct (myTicket : int list) (departureIndices : (int * string) list) : int64 = 
+        let indicesToCheck = 
+            departureIndices
+            |> List.filter (fun (_, fieldName) ->
+                fieldName.StartsWith("departure")
+                )
+            |> List.map fst
+        indicesToCheck
+        |> List.map (fun x -> myTicket |> List.item x)
+        |> List.map int64
+        |> List.product
+
+    let parse (fileInput : string list) =
+        let splitOnComma (inputString : string) =
+            let getNumsRegex = new Regex("\d{1,}")
+            inputString
+            |> getNumsRegex.Matches
+            :> System.Collections.Generic.IList<Match>
+            |> List.ofSeq
+            |> List.map (fun x -> x.Value)
+            |> List.map Int32.Parse
+
+        let getTicketRuleFromString (inputString : string) =
+            let getAllToColonRegex = new Regex(".+?(?=:)")
+            let fieldName = inputString |> getAllToColonRegex.Match |> (fun x -> x.Value)
+            let getMatchAsInt (item : int) = 
+                let getNumsRegex = new Regex("\d{1,}")
+                let matches = inputString |> getNumsRegex.Matches
+                matches.Item(item).Value |> Int32.Parse
+            let firstRange = Range<int>(getMatchAsInt 0, getMatchAsInt 1)
+            let secondRange = Range<int>(getMatchAsInt 2, getMatchAsInt 3)
+            (fieldName, (firstRange, secondRange))
+
+        let inputStringList = fileInput |> Advent2020.General.groupedStrings
+
+        let ruleStrings = inputStringList.Item 0
+        let myTicketString = inputStringList.Item 1
+        let nearbyTicketStrings = inputStringList.Item 2
+
+        let myTicket = 
+            myTicketString
+            |> List.tail
+            |> List.exactlyOne
+            |> splitOnComma
+
+        let nearbyTickets = 
+            nearbyTicketStrings
+            |> List.tail
+            |> List.map splitOnComma
+
+        let rules = ruleStrings |> List.map getTicketRuleFromString |> Map.ofList
+
+        //printfn "My Ticket: %A" myTicket
+        //printfn "Nearby Ticket: %A" nearbyTickets
+        //printfn "Rules: %A" rules
+
+        (rules, myTicket, nearbyTickets)
+
+    let run : unit = 
+        let fileName = "Advent2020D16.txt"
+        let fileInput = Advent2020.File.listedLines fileName
+        let (rules, myTicket, nearbyTickets) = parse fileInput
+        
+        let departureIndices = ((rules, nearbyTickets) ||> findValidTickets) |> (matchFieldsToRules rules)
+        let product = departureProduct myTicket departureIndices
+
+        printfn "Product: %i" product
+        // printfn "Ticket Scanning Error Rate: %A" ((rules, nearbyTickets) ||> findValidTickets)
+
+        //printfn "Sum: %i" (countQuestionsAnswerByAll initialState)
---
+++ Advent2020D7/General.fs
@@ -0,0 +1,101 @@
+﻿namespace Advent2020
+
+open System
+
+/// <summary>Holds a lesser and greater number, and methods to use with them.</summary>
+/// <param name="lower">The lesser value.</param>
+///<param name="upper">The greater value.</param>
+[<StructAttribute>]
+type Range<'T>(lower : 'T when 'T : comparison, upper : 'T) =
+    member this.Lower = lower
+    member this.Upper = upper
+
+    /// <summary>
+    /// Checks whether a value is in range of the parameters, inclusively.
+    /// Values that are equal to a side of the range are considered in range.
+    /// </summary>
+    /// <param name="valToCheck">Value to check whether it is in range.</param>
+    member this.InRangeInclusive (valToCheck : 'T) =
+        (valToCheck >= this.Lower) && (valToCheck <= this.Upper)
+
+    /// <summary>
+    /// Checks whether a value is in range of the parameters, inclusively.
+    /// Values that are equal to a side of the range are considered NOT in range.
+    /// </summary>
+    /// <param name="valToCheck">Value to check whether it is in range.</param>
+    member this.InRangeExclusive (valToCheck : 'T) =
+        (valToCheck > this.Lower) && (valToCheck < this.Upper)
+
+module Range =
+    let InRangeInclusive (baseRange : Range<'T>) (valToCheck : 'T) =
+        baseRange.InRangeInclusive(valToCheck)
+    let InRangeExclusive (baseRange : Range<'T>) (valToCheck : 'T) =
+        baseRange.InRangeExclusive(valToCheck)
+
+module Option =
+    let (>>=) r f = Option.bind f r
+    let rtn v     = Some v
+     
+    let traverseList f ls = 
+        let folder head tail = f head >>= (fun h -> tail >>= (fun t -> h::t |> rtn))
+        List.foldBack folder ls (rtn List.empty)
+    let sequenceList ls = traverseList id ls
+    // val traverseList : ('a -> 'b option) -> 'a list -> 'b list option
+    // val sequenceList : 'a option list -> 'a list option
+
+module List = 
+    let product (list : int64 list) : int64 = 
+        let rec product (state : int64) (remainingList : int64 list) = 
+            match remainingList with 
+            | [] -> state
+            | head::tail ->
+                let newState = state * head
+                product newState tail
+        product (list.Head) list.Tail
+
+module File =
+
+    let userProfile = Environment.GetEnvironmentVariable("USERPROFILE")
+    let downloadLocation = userProfile + @"\" + "Downloads"
+    let getFullFilePath fileName = 
+        downloadLocation + @"\" + fileName
+
+    //Text Import
+    let readLines (filePath:string) = seq {
+        use sr = new System.IO.StreamReader (filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine ()
+    }
+
+    let listLines (filePath:string) = readLines filePath |> Seq.toList
+
+    let listedLines fileName = getFullFilePath fileName |> listLines
+
+module General = 
+
+    let flip f a b = f b a
+
+    let groupedStrings (rawInput : string list) : string list list = 
+        let inputFolder (acc : string list list) (entryToAdd : string) : string list list =
+            match entryToAdd with 
+            | "" ->
+                let emptyStringList = List<string>.Empty //Note that this is a property with capital E of List<string>
+                let newEmptyStringList = List.singleton emptyStringList
+                List.append acc newEmptyStringList
+            | entryToAdd ->
+                let lastStringListSplitIndex = 
+                    ((acc |> List.length) - 1)
+                let front, last = acc |> List.splitAt lastStringListSplitIndex
+                let updateFinalString (stringListListToReplace : string list list) : string list list = 
+                    stringListListToReplace
+                    |> List.head
+                    |> (flip List.append) (List.singleton entryToAdd)
+                    |> List.singleton
+                last
+                |> updateFinalString
+                |> List.append front
+        let initialAcc : string list list =
+            List.empty<string>  //While this is a function which takes a string type, it does the same thing.
+            |> List.singleton
+        (initialAcc, rawInput)
+        ||> List.fold inputFolder
