cee04e4c50d4058241da9b5c7423f255384380cb
Robert Zanone <rjzanone@gmail.com>
2021-11-22 08:22:07 -0500

    Initial setup

---
+++ Advent2020D7/Day15.fs
@@ -0,0 +1,17 @@
+﻿namespace Advent2020.Day15
+
+open System
+open Advent2020 //Contains Range type and accessors
+open System.Text.RegularExpressions //Allows usage of Regular Expressions
+
+module Main =
+
+    let parse (fileInput : string list) =
+        fileInput
+
+    let run : unit = 
+        let fileName = "Advent2020D16.txt"
+        let fileInput = Advent2020.File.listedLines fileName
+        let initialState = parse fileInput
+
+        printfn "Test"
---
+++ Advent2020D7/Day16 - Copy.fs
@@ -0,0 +1,18 @@
+﻿namespace Advent2020.Day15
+
+open System
+open Advent2020
+open System.Text.RegularExpressions //Allows usage of Regular Expressions
+open HPCsharp
+
+module Main =
+
+    let parse (fileInput : string list) =
+        fileInput
+
+    let run : unit = 
+        let fileName = "Advent2020D15.txt"
+        let fileInput = Advent2020.File.listedLines fileName
+        let initialState = parse fileInput
+
+        printfn "Test"
