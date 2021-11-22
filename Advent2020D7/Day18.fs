e684f8b2653cc682c0e6036050469aadbf402101
Robert Zanone <rjzanone@gmail.com>
2021-11-21 19:14:56 -0500

    Day 18 Part 1 and 2 since they're very close.

--- Advent2020D7/Day18.fs
+++ Advent2020D7/Day18.fs
@@ -1,176 +1,105 @@
-﻿namespace Advent2020.Day18
+﻿
+
+namespace Advent2020.Day18
 
 open System
 open Advent2020 //Contains Range type and accessors
 open System.Text.RegularExpressions //Allows usage of Regular Expressions
+open FParsec
+
+type calcChar =
+    | LeftParenth
+    | RightParenth
+    | Mult
+    | Add
+    | Val of int
+
+type expressionEntry = 
+    | Character of calcChar
+    | Expression of expressionEntry list
+
+type Operator = Add | Mul
+type OperationData = {operator : Operator; left : Expression; right : Expression}
+and Expression = 
+    | Value of int64
+    | Operation of OperationData
+    | Parentheses of Expression
 
 module Main =
 
-    let findTicketScanningErrorRate (rules : Map<string,(Range<int> * Range<int>)>) (nearbyTickets : int list list) : int =
-        let allValidRanges : Range<int> list =
-            let (range1, range2) = 
-                rules
-                |> Map.toList
-                |> List.map snd
-                |> List.unzip
-            range2
-            |> List.append range1
+    let fromInternet (input : string array) = 
+        
+        // parsers
+        let parseUInt64 : Parser<uint64, unit> = puint64 |>> uint64
+        let removeWhitespace s = pstring s >>. spaces
+        // Function that will take in the string s and parse for it.  Then, we will run the parsed result into a parser which removes whitespace, and return the new result.
+        // This function will handle the removal of whitespace around characters we parse, and return the string after removal.
 
-        let allNearbyTicketFields = 
-            nearbyTickets
-            |> List.concat
+        // We will make a bigger parsing function which consumes an expression.
+        // This will take our integer parser, and remove any whitespace before it.
+        // If there is no int to parse, then we will perform a different parse check.
+        // We will parse for an opening parenthesis, then a closing parenthesis, and finally an expression parse, and return the parsed result from after the closing parenthesis.
+        let parseTerm expression = (parseUInt64 .>> spaces) <|> between (removeWhitespace "(") (removeWhitespace ")") expression
+        
+        //Create a function which will run the parser and return the appropriate type
+        let runParser expr str =
+            //run is a method from FParsec, and quickly runs the parser with basic parameters on the string provided.
+            match run expr str with
+            | Success (result, _ , _) -> result
+            | Failure (errorMsg, _, _) -> failwithf "Error from parser: %s" errorMsg
+        
 
-        let checkIfFieldIsValid (rangesList : Range<int> list) (field : int) : bool = 
-            rangesList
-            |> List.map (fun x -> x.InRangeInclusive field)
-            |> List.contains true
-            
-        let valueForValidFields (field : int) = 
-            let validity = checkIfFieldIsValid allValidRanges field
-            if validity then 0 else field
+        let runPart input =
+            let opp = OperatorPrecedenceParser<uint64, unit, unit>()
+            let expression = opp.ExpressionParser //The expression parser.  
+        
+            let addOperator = InfixOperator ("+", spaces, 2, Associativity.Left, (+)) //Precedence 2 is for part 2, where addition is evaluated first
+            //Infix Operator is FParsec's binary infix operator definition.  We can use this to create the Parsers.
+            //Whenever we see a + sign, we shall ignore spaces near it.  It shall be run at level 1 precedence (basically order of operations).
+            //Solve from left to right.
+            //When you see a "+", the operation to perform is the + operation.
+            let multOperator = InfixOperator ("*", spaces, 1, Associativity.Left, (*))
+            // Multiplication with level 1 precedence will be evaluated at the same time as addition.
+        
+            opp.TermParser <- parseTerm expression
+            // This parser is called to parse the terms in between the operators.
+            // There is no default, so you must set this parser before you can call the ExpressionParser.
+            // Note that the term parser is also expected to parse any whitespace after a term. 
+            opp.AddOperator(addOperator)
+            opp.AddOperator(multOperator)
+        
+            input
+            |> Array.sumBy (runParser expression) 
+            |> printfn "%A"
+        
+        runPart input
+        
 
-        allNearbyTicketFields
-        |> List.map valueForValidFields
-        |> List.sum
+    //let solveExpressionEntry (inputEntries : expressionEntry list) : int = 
+        
 
-    let findValidTickets (rules : Map<string,(Range<int> * Range<int>)>) (nearbyTickets : int list list) =
-        let allValidRanges : Range<int> list =
-            let (range1, range2) = 
-                rules
-                |> Map.toList
-                |> List.map snd
-                |> List.unzip
-            range2
-            |> List.append range1
-
-        let checkIfFieldIsValid (rangesList : Range<int> list) (field : int) : bool = 
-            rangesList
-            |> List.map (fun x -> x.InRangeInclusive field)
-            |> List.contains true
-
-        nearbyTickets
-        |> List.filter (fun x ->
-            x
-            |> List.map (checkIfFieldIsValid allValidRanges)
-            |> List.forall id
-            )
-
-    let matchFieldsToRules (rules : Map<string,(Range<int> * Range<int>)>) (validTickets : int list list) = // : string Set list = 
-        let ruleEntries = validTickets |> List.transpose //This is a grouping of all entries in an individual rule
-        //We will need to find which entry correlates to each grouping of rules
-        let findRulesTheEntriesMatch (entries : int list) : string Set =
-            let entryValidRules (entry : int) = 
-                rules
-                |> Map.filter(fun _ ranges ->
-                    let range1, range2 = ranges
-                    range1.InRangeInclusive(entry) || range2.InRangeInclusive(entry)
-                    )
-                |> Map.keys
-                |> Set.ofSeq
-            entries
-            |> List.map entryValidRules
-            |> Set.intersectMany
-
-        let validRuleConfigs = 
-            ruleEntries
-            |> List.map findRulesTheEntriesMatch
-
-        let getAllSingletonEntries (validRuleConfigs : string Set list) : string list =
-            let ruleMatchInit = validRuleConfigs |> List.map (fun _ -> "") //Empty list of strings to hold the field name
-            let rec removeSingletonElements (currentValidRules : string list) (currentPossibleRules : string Set list) = 
-                let filledList = currentValidRules |> List.contains ""
-                match filledList with 
-                | false -> (currentValidRules, currentPossibleRules)
-                | true ->
-                    let singletonRuleIndex = 
-                        currentPossibleRules
-                        |> List.findIndex (fun x -> x.Count = 1)
-                    let element =
-                        currentPossibleRules
-                        |> List.item singletonRuleIndex
-                        |> Set.toList
-                        |> List.exactlyOne
-                    let newValidRules =
-                        currentValidRules
-                        |> List.updateAt singletonRuleIndex element
-                    let newCurrentPossibleRules = 
-                        currentPossibleRules
-                        |> List.map (Set.remove element)
-                    removeSingletonElements newValidRules newCurrentPossibleRules
-            let (rulesList, _) = removeSingletonElements ruleMatchInit validRuleConfigs
-            rulesList
-
-        //printfn "%A" (getAllSingletonEntries validRuleConfigs)
-
-        getAllSingletonEntries validRuleConfigs |> List.indexed
-
-    let departureProduct (myTicket : int list) (departureIndices : (int * string) list) : int64 = 
-        let indicesToCheck = 
-            departureIndices
-            |> List.filter (fun (_, fieldName) ->
-                fieldName.StartsWith("departure")
+    let parse (fileInput : string list) : calcChar list list =
+        let stringToCalcCharList (inputString : string) = 
+            let noWhitespace = inputString.Replace(" ", String.Empty)
+            let charOrder = noWhitespace.ToCharArray() |> List.ofArray
+            charOrder
+            |> List.map (fun x ->
+                match x with
+                | '(' -> LeftParenth
+                | ')' -> RightParenth
+                | '*' -> Mult
+                | '+' -> calcChar.Add
+                | x -> Val(Int32.Parse(x.ToString()))
                 )
-            |> List.map fst
-        indicesToCheck
-        |> List.map (fun x -> myTicket |> List.item x)
-        |> List.map int64
-        |> List.product
-
-    let parse (fileInput : string list) =
-        let splitOnComma (inputString : string) =
-            let getNumsRegex = new Regex("\d{1,}")
-            inputString
-            |> getNumsRegex.Matches
-            :> System.Collections.Generic.IList<Match>
-            |> List.ofSeq
-            |> List.map (fun x -> x.Value)
-            |> List.map Int32.Parse
-
-        let getTicketRuleFromString (inputString : string) =
-            let getAllToColonRegex = new Regex(".+?(?=:)")
-            let fieldName = inputString |> getAllToColonRegex.Match |> (fun x -> x.Value)
-            let getMatchAsInt (item : int) = 
-                let getNumsRegex = new Regex("\d{1,}")
-                let matches = inputString |> getNumsRegex.Matches
-                matches.Item(item).Value |> Int32.Parse
-            let firstRange = Range<int>(getMatchAsInt 0, getMatchAsInt 1)
-            let secondRange = Range<int>(getMatchAsInt 2, getMatchAsInt 3)
-            (fieldName, (firstRange, secondRange))
-
-        let inputStringList = fileInput |> Advent2020.General.groupedStrings
-
-        let ruleStrings = inputStringList.Item 0
-        let myTicketString = inputStringList.Item 1
-        let nearbyTicketStrings = inputStringList.Item 2
-
-        let myTicket = 
-            myTicketString
-            |> List.tail
-            |> List.exactlyOne
-            |> splitOnComma
-
-        let nearbyTickets = 
-            nearbyTicketStrings
-            |> List.tail
-            |> List.map splitOnComma
-
-        let rules = ruleStrings |> List.map getTicketRuleFromString |> Map.ofList
-
-        //printfn "My Ticket: %A" myTicket
-        //printfn "Nearby Ticket: %A" nearbyTickets
-        //printfn "Rules: %A" rules
-
-        (rules, myTicket, nearbyTickets)
+        fileInput
+        |> List.map stringToCalcCharList
 
     let run : unit = 
-        let fileName = "Advent2020D16.txt"
+        let fileName = "Advent2020D18.txt"
         let fileInput = Advent2020.File.listedLines fileName
-        let (rules, myTicket, nearbyTickets) = parse fileInput
-        
-        let departureIndices = ((rules, nearbyTickets) ||> findValidTickets) |> (matchFieldsToRules rules)
-        let product = departureProduct myTicket departureIndices
 
-        printfn "Product: %i" product
-        // printfn "Ticket Scanning Error Rate: %A" ((rules, nearbyTickets) ||> findValidTickets)
+        fileInput
+        |> Array.ofList
+        |> fromInternet
 
-        //printfn "Sum: %i" (countQuestionsAnswerByAll initialState)
+        // printfn "Value: %i" value
