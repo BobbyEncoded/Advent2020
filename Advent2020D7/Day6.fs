cab30fcb1c43d76c2e939b31d80c3d3cc8d0c6a4
Robert Zanone <rjzanone@gmail.com>
2021-11-23 14:10:27 -0500

    Day 6 Cleanup complete

--- Advent2020D7/Day6.fs
+++ Advent2020D7/Day6.fs
@@ -1,70 +1,26 @@
 ﻿namespace Advent2020.Day6
 
 open System
-
-type seat = 
-    {
-        row : int;
-        col : int;
-    }
-
-
-module Option =
-    let (>>=) r f = Option.bind f r
-    let rtn v     = Some v
-     
-    let traverseList f ls = 
-        let folder head tail = f head >>= (fun h -> tail >>= (fun t -> h::t |> rtn))
-        List.foldBack folder ls (rtn List.empty)
-    let sequenceList ls = traverseList id ls
-    // val traverseList : ('a -> 'b option) -> 'a list -> 'b list option
-    // val sequenceList : 'a option list -> 'a list option
+open Advent2020.General
 
 module Main =
 
-    let countUniqueQuestionsAnswer (inputAnswers : char Set list list) : int = 
+    let handleAnswers (inputAnswers : char Set list list) (setArrangementFunc : seq<Set<char>> -> Set<char>) : int = 
         inputAnswers
         |> List.map (fun f ->
             f
-            |> Set.unionMany
+            |> setArrangementFunc
             |> Set.count
             )
         |> List.sum
+
+    let countUniqueQuestionsAnswer (inputAnswers : char Set list list) : int = 
+        handleAnswers inputAnswers Set.unionMany
 
     let countQuestionsAnswerByAll (inputAnswers : char Set list list) : int = 
-        inputAnswers
-        |> List.map (fun f ->
-            f
-            |> Set.intersectMany
-            |> Set.count
-            )
-        |> List.sum
+        handleAnswers inputAnswers Set.intersectMany
 
     let parse (fileInput : string list) : char Set list list =
-        let groupedStrings (rawInput : string list) : string list list = 
-            let inputFolder (acc : string list list) (entryToAdd : string) : string list list =
-                match entryToAdd with 
-                | "" ->
-                    let emptyStringList = List<string>.Empty //Note that this is a property with capital E of List<string>
-                    let newEmptyStringList = List.singleton emptyStringList
-                    List.append acc newEmptyStringList
-                | entryToAdd ->
-                    let lastStringListSplitIndex = 
-                        ((acc |> List.length) - 1)
-                    let front, last = acc |> List.splitAt lastStringListSplitIndex
-                    let updateFinalString (stringListListToReplace : string list list) : string list list = 
-                        stringListListToReplace
-                        |> List.head
-                        |> List.append (List.singleton entryToAdd)
-                        |> List.singleton
-                    last
-                    |> updateFinalString
-                    |> List.append front
-            let initialAcc : string list list =
-                List.empty<string>  //While this is a function which takes a string type, it does the same thing.
-                |> List.singleton
-            (initialAcc, rawInput)
-            ||> List.fold inputFolder
         fileInput
         |> groupedStrings
         |> List.map (fun f ->
@@ -75,12 +31,9 @@ module Main =
                 )
             )
 
-    
-
     let run : unit = 
         let fileName = "Advent2020D6.txt"
         let fileInput = Advent2020.File.listedLines fileName
         let initialState = parse fileInput
 
-        //alternateMissingID initialState
         printfn "Sum: %i" (countQuestionsAnswerByAll initialState)
