0e556f759314ba10e9597e63dacdbbb53e9864f4
Robert Zanone <rjzanone@gmail.com>
2021-11-24 16:05:45 -0500

    Initial commit

---
+++ Advent2020D7/Day20.fs
@@ -0,0 +1,18 @@
+﻿namespace Advent2020.Day20
+
+open System
+open Advent2020 //Contains Range type and accessors
+open System.Text.RegularExpressions //Allows usage of Regular Expressions
+open System.Linq        
+
+module Main =
+
+    let parse (fileInput : string list) =
+        fileInput
+
+    let run : unit = 
+        let fileName = "Advent2020D20.txt"
+        let fileInput = Advent2020.File.listedLines fileName
+        let initialState = parse fileInput
+
+        printfn "Test"
