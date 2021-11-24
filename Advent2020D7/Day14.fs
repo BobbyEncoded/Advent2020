50724f30737947d1fb954e20724298b8f6f0f3da
Robert Zanone <rjzanone@gmail.com>
2021-11-24 08:30:12 -0500

    Initial setup of Day 14

---
+++ Advent2020D7/Day14.fs
@@ -0,0 +1,17 @@
+﻿namespace Advent2020.Day14
+
+open System
+open Advent2020 //Contains Range type and accessors
+open System.Text.RegularExpressions //Allows usage of Regular Expressions
+
+module Main =
+
+    let parse (fileInput : string list) =
+        fileInput
+
+    let run : unit = 
+        let fileName = "Advent2020D14.txt"
+        let fileInput = Advent2020.File.listedLines fileName
+        let initalState = parse fileInput
+
+        printfn "Test"
