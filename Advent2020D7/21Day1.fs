a5a80d279166cfb24912ca7b6239dd5439572a78
bobman192 <bobman192@gmail.com>
2021-12-01 07:11:00 -0500

    P2 complete

--- Advent2020D7/21Day1.fs
+++ Advent2020D7/21Day1.fs
@@ -19,11 +19,23 @@ module Main =
         |> List.map convertWindowToPlus
         |> List.sum
 
+    let part2 (initInts : int list) = 
+        let windows = initInts |> List.windowed 3
+        let windowSums = 
+            windows
+            |> List.map List.sum
+        let windowWindows = windowSums |> List.windowed 2
+        let convertWindowToPlus (inputList : int list) = 
+            if inputList.[1] > inputList.[0] then 1 else 0
+        windowWindows
+        |> List.map convertWindowToPlus
+        |> List.sum
+
     let run : unit = 
         let fileName = "Advent2021D1.txt"
         let fileInput = Advent2020.File.listedLines fileName
         let initialState = parse fileInput
 
-        let part1ans = initialState |> part1
+        let part1ans = initialState |> part2
 
         printfn "%i" part1ans
