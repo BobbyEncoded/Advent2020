70e642e200797e3edff352e1b36ad3eb16e25d0d
bobman192 <bobman192@gmail.com>
2021-12-01 07:09:11 -0500

    Part 1 complete

---
+++ Advent2020D7/21Day1.fs
@@ -0,0 +1,29 @@
+﻿namespace Advent2021.Day1
+
+open System
+open Advent2020 //Contains Range type and accessors
+open System.Text.RegularExpressions //Allows usage of Regular Expressions
+open System.Linq        
+
+module Main =
+
+    let parse (fileInput : string list) =
+        fileInput
+        |> List.map Int32.Parse
+
+    let part1 (initInts : int list) = 
+        let windows = initInts |> List.windowed 2
+        let convertWindowToPlus (inputList : int list) = 
+            if inputList.[1] > inputList.[0] then 1 else 0
+        windows
+        |> List.map convertWindowToPlus
+        |> List.sum
+
+    let run : unit = 
+        let fileName = "Advent2021D1.txt"
+        let fileInput = Advent2020.File.listedLines fileName
+        let initialState = parse fileInput
+
+        let part1ans = initialState |> part1
+
+        printfn "%i" part1ans
--- Advent2020D7/main.fs
+++ Advent2020D7/main.fs
@@ -7,7 +7,7 @@ module Program =
     [<EntryPoint>]
     let main argv = 
         let timer = System.Diagnostics.Stopwatch.StartNew()
-        Advent2020.Day20.Main.run
+        Advent2021.Day1.Main.run
         timer.Stop()
         printfn "The program took %i ms to run." timer.ElapsedMilliseconds
         0
